10eac69550485d58fe6374e093ca41501081289a
Cleophas <cleophasfoy@gmail.com>
2022-09-06 13:52:29 -0500

    added random number

--- app.js
+++ app.js
@@ -6,5 +6,11 @@ let userChoice
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click',(e) => {
   userChoice = e.target.id
-  userChoiceDisplay.innerHTML = userChoice 
+  userChoiceDisplay.innerHTML = userChoice
+  generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+  console.log(randomNumber)
+}
